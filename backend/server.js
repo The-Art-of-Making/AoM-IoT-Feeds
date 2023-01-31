2ec58c4141d703bc91c320af7a46a11296bff50f
delta-12 <btesq235@gmail.com>
2023-01-31 18:09:54 -0500

    backend servers frontend

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,11 @@ server.use(bodyParser.json())
 
 server.use("/feeds/", feeds)
 
+server.use(express.static("../frontend/build"))
+server.get("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+})
+
 const port = process.env.PORT || 5000
 
 server.listen(port, () => console.log(`Server up and running on port ${port}!`))
